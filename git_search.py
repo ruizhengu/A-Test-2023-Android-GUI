4c658a521b0073b40b3301c9582461a8f4112d2d
guruizhen <ravenna1031@gmail.com>
2022-10-12 13:59:23 +0100

    add apps dataset

--- git_search.py
+++ git_search.py
@@ -1,28 +1,19 @@
-import time
-
-import github.GithubException
-
 from git_token import GITHUB_TOKEN
 from github import Github
-from write_csv import Writer
 
-g = Github(GITHUB_TOKEN)
 
-# search keywords
-keywords = ["androidx.test.uiautomator", "androidx.test.espresso"]
-# search in build.gradle
-languages = ["language:" + language for language in ["Gradle"]]
-keywords.extend(languages)
-query = " ".join(keywords)
-print(query)
+class Search:
+    def __init__(self):
+        self.g = Github(GITHUB_TOKEN)
 
-writer = Writer()
-results = g.search_code(query)
+    def search_repo(self, name: list):
+        name.append("in:name")
+        query = " ".join(name)
+        result = self.g.search_repositories(query)
+        print(result.totalCount)
 
-counter = 0
-for result in results:
-    counter += 1
-    if counter % 30 == 0:
-        time.sleep(60)
-    writer.write_row(result.repository.full_name, result.path, result.repository.stargazers_count)
-    print(result.repository.stargazers_count)
+
+if __name__ == '__main__':
+    search = Search()
+    names = ["0xpr03/VocableTrainer-Android"]
+    search.search_repo(names)
