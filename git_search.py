f70b01698d0afeadcebbe8e24e7d9732f578da4e
guruizhen <ravenna1031@gmail.com>
2022-10-13 12:04:55 +0100

    get exist repos

--- git_search.py
+++ git_search.py
@@ -1,19 +1,52 @@
+import time
+
+import github
+
 from git_token import GITHUB_TOKEN
 from github import Github
+from utils import *
 
 
 class Search:
     def __init__(self):
         self.g = Github(GITHUB_TOKEN)
 
-    def search_repo(self, name: list):
-        name.append("in:name")
-        query = " ".join(name)
-        result = self.g.search_repositories(query)
-        print(result.totalCount)
+    # def search_repo(self, name: list):
+    #     name.append("in:name")
+    #     query = " ".join(name)
+    #     result = self.g.search_repositories(query)
+    #     print(result.totalCount)
+
+    # get repos from excel
+    @staticmethod
+    def get_repos():
+        repo_filter()
+        repo_data = pd.read_excel(data_path, sheet_name=sheet_repositories)
+        data_list = repo_data.values.tolist()
+        return data_list
+
+    # dose the repo with the name exists
+    def repos_exist(self):
+        repos = self.get_repos()
+        for repo in repos:
+            try:
+                query = f"repo:{repo[0]}"
+                result = self.g.search_repositories(query)
+                repo.append(result.totalCount)
+                # repo.append(1)
+            except github.GithubException as e:
+                if e.data["message"] == "Validation Failed":
+                    repo.append(0)
+                else:
+                    print(repo)
+                    print(e.data["message"])
+                    time.sleep(60)
+                continue
+        exist_data = pd.DataFrame(repos, columns=["repository", "count"])
+        with pd.ExcelWriter(data_path, mode="a") as writer:
+            exist_data.to_excel(writer, sheet_name=sheet_exist, index=False)
 
 
 if __name__ == '__main__':
     search = Search()
-    names = ["0xpr03/VocableTrainer-Android"]
-    search.search_repo(names)
+    search.repos_exist()
--- utils.py
+++ utils.py
@@ -1,1 +1,24 @@
-data_file = "gui_repos.xlsx"
+import json
+import pandas as pd
+
+data_path = "gui_repos.xlsx"
+# data_writer = pd.ExcelWriter(data_path)
+sheet_repositories = "repositories"
+sheet_exist = "exist"
+
+
+def repo_filter():
+    f = open("repos.json")
+    data = json.load(f)
+    gui_repos = []
+
+    for repo in data:
+        attr = data[repo]
+        if attr["num_androidTests"] != 0:
+            gui_repos.append(repo)
+
+    gui_data = pd.DataFrame(gui_repos, columns=["repository"])
+    with pd.ExcelWriter(data_path) as writer:
+        gui_data.to_excel(writer, sheet_name=sheet_repositories, index=False)
+
+# repo_filter()
