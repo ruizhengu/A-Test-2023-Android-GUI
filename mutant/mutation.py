40eb2f64e40e37511380b674c0bcc337f4d6098b
guruizhen <ravenna1031@gmail.com>
2022-12-07 17:51:24 +0000

    mutation-run test script

--- mutant/mutation.py
+++ mutant/mutation.py
@@ -3,27 +3,35 @@ from os import listdir, path
 from distutils.dir_util import copy_tree
 import subprocess
 
-android_path = "../../../Experiment/ShiftCal"
-src_main_path = path.join(android_path, "app", "src", "main")
-# shutil.rmtree(src_main_path)
-mutant_path = "Mutant_ShiftCal"
 
-mutants = [m for m in listdir(mutant_path) if "log" not in m]
+class Mutation:
+    def __init__(self):
+        self.app_path = "../../../Experiment/ShiftCal"
+        self.app_main_path = path.join(self.app_path, "app", "src", "main")
+        self.mutant_path = "Mutant_ShiftCal"
+        self.mutants = [m for m in listdir(self.mutant_path) if "log" not in m]
+        self.test_script = "sh runJacoco.sh"
 
-t = path.join(mutant_path, mutants[0])
-print(t)
+    def replace_app_main(self, mutant_main):
+        shutil.rmtree(self.app_main_path)
+        mutant_main_path = path.join(self.mutant_path, mutant_main)
+        copy_tree(mutant_main_path, self.app_main_path)
 
-copy_tree(t, src_main_path)
+    def kill_mutants(self):
+        for mutant in self.mutants:
+            print(f"mutant: {mutant}")
+            self.replace_app_main(mutant)
+            process = subprocess.Popen(self.test_script, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            output, err = process.communicate()
+            if "BUILD SUCCESSFUL" in output.decode("utf-8"):
+                print("mutant killed")
+            else:
+                print(f"output: {output.decode('utf-8')}")
+                print(f"err: {err.decode('utf-8')}")
+                print("mutant not killed")
+            break
 
-test_command = android_path + "/gradlew jacocoTestReportDebug"
-print(test_command)
 
-def test_command_result():
-    process = subprocess.Popen(test_command, shell=True, stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
-    return process
-
-
-with test_command_result():
-    for line in test_command_result().stdout:
-        print(line)
+if __name__ == '__main__':
+    mutation = Mutation()
+    mutation.kill_mutants()
